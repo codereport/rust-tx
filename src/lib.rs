32570b4ac5593f62fb55ec7fa3ecc32db7808ca3
Conor Hoekstra <codereport@outlook.com>
2022-10-23 23:03:24 -0400

    Add commented out tests that don't work

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,34 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+// pub fn check_matrix(Tensor grid) {
+//     grid.eye()
+//         .s(rev, id)
+//         .equal(grid.min(1))
+// }
+
+// pub fn count_negatives(Tensor matrix) {
+//     matrix.lt(0).sum()
+// }
+
+// pub fn max_wealth(Tensor accounts) {
+//     // what to call max reduce? probably maximum
+//     accounts.sum(1).maximum()
+// }
+
+// pub fn mco(Tensor vector) {
+//     vector.scan(phi1(left, mul, plus))
+//           .maximum()
+// }
+
+// pub fn max_paren_depth(str equation) {
+//     equation.to_tensor()
+//             .outer("()")
+//             .reduce(num::minus, 1)
+//             .scan(num::plus)
+//             .maximum()
+// }
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -11,4 +39,5 @@ mod tests {
         let result = add(2, 2);
         assert_eq!(result, 4);
     }
+
 }
